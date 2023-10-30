a2842df06b3dc0220f13cac8e0e03f0ed3b8dbfe
Nick Chen <nick.chen@cmp.com.tw>
2023-10-30 14:38:19 +0800

    Initial commit

---
+++ src/main/java/tw/niq/api/NiqApiApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiqApiApplication.class, args);
+	}
+
+}
