7b35fec39d38129026e50e93717dfee69c45980f
Nick Chen <nick.chen@cmp.com.tw>
2023-11-08 11:22:12 +0800

    Remove springdoc

--- src/main/java/tw/niq/api/controller/UserController.java
+++ src/main/java/tw/niq/api/controller/UserController.java
@@ -2,6 +2,7 @@ package tw.niq.api.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,31 +17,33 @@ import tw.niq.api.model.UserModel;
 public class UserController {
 
 	public static final String PATH = "/api/v1/users";
-	public static final String PATH_USER_ID = "/api/v1/users/{userId}";
+	public static final String PATH_USER_ID = "/{userId}";
 	
 	@GetMapping
-	public String getUsers() {
-		return "GET";
+	public String getUsers(
+			@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "25") int limit) {
+		return String.format("GET - page: %s, limit: %s", page, limit);
 	}
 	
 	@GetMapping(path = PATH_USER_ID)
-	public String getUserByUserId(@RequestParam("userId") String userId) {
-		return "GET: " + userId;
+	public String getUserByUserId(@PathVariable("userId") String userId) {
+		return String.format("GET - userId: %s", userId);
 	}
 	
 	@PostMapping
 	public String createUser(@RequestBody UserModel userModel) {
-		return "POST: " + userModel;
+		return String.format("POST - userModel: %s", userModel);
 	}
 	
 	@PutMapping(path = PATH_USER_ID)
-	public String updateUserByUserId(@RequestParam("userId") String userId, @RequestBody UserModel userModel) {
-		return "PUT: " + userId + ", " + userModel;
+	public String updateUserByUserId(@PathVariable("userId") String userId, @RequestBody UserModel userModel) {
+		return String.format("PUT - userId: %s, userModel: %s", userId, userModel);
 	}
 	
 	@DeleteMapping(path = PATH_USER_ID)
-	public String deleteUserByUserId(@RequestParam("userId") String userId) {
-		return "DELETE: " + userId;
+	public String deleteUserByUserId(@PathVariable("userId") String userId) {
+		return String.format("DELETE - userId: %s", userId);
 	}
 	
 }
